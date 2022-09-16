f0a9f6a747bcce161a8e9b55ddcc6ecfb7d94501
Ryanpratama01 <113703678+Ryanpratama01@users.noreply.github.com>
2022-09-16 08:44:43 -0700

    Add files via upload

---
+++ 220441100098_RyanPratamaPutra_rumusbangunruang_Pelcod.py
@@ -0,0 +1,18 @@
+#rumus balok
+p=int(input('masukan panjang balok:'))
+l=int(input('masukan lebar balok:'))
+t=int(input('masukan tinggi balok:'))
+
+volume1 = p * l * t
+print ("jadi volume balok adalah:", volume1)
+
+#rumus kubus
+s=int(input('masukan sisi 1:'))
+s=int(input('masukan sisi 2:'))
+s=int(input('masukan sisi 3:'))
+
+volume2 = s * s * s
+print ("jadi volume kubus adala:", volume2)
+
+#nama : Ryan Pratama Putra
+#NIM : 220441100098
